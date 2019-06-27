f8c814898266e24045b68676f94d6b2e6bf57649
Dimanikin <driver-s@mail.ru>
2019-06-27 14:31:42 +0200

    Add MARKs

---
+++ myCurrency/Contents.swift
@@ -0,0 +1,12 @@
+//
+//  Contents.swift
+//  myCurrency
+//
+//  Created by Dima on 16/06/2019.
+//  Copyright © 2019 Dima Anikin. All rights reserved.
+//
+
+struct Contents {
+    static let urlString = "https://www.cbr-xml-daily.ru/daily_json.js"
+}
+
--- myCurrency/Model.swift
+++ myCurrency/Model.swift
@@ -9,21 +9,59 @@
 import Foundation
 
 struct DailyCurrency: Decodable {
-    //    let Date: String
-    //    let PreviousDate: String
-    //    let PreviousURL: String
-    //    let Timestamp: String
-    let Valute: Valute
+    
+    let valute: Valute
+    
+    enum CodingKeys: String, CodingKey {
+        case valute = "Valute"
+    }
+    
+    init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        valute = try container.decode(Valute.self, forKey: .valute)
+    }
 }
 
 struct Valute: Decodable {
-    let USD, EUR, SGD: TypeCurrency
+    
+    let usd, eur, sgd: TypeCurrency
+    
+    enum CodingKeys: String, CodingKey {
+        case usd = "USD"
+        case eur = "EUR"
+        case sgd = "SGD"
+    }
+    
+    init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        usd = try container.decode(TypeCurrency.self, forKey: .usd)
+        eur = try container.decode(TypeCurrency.self, forKey: .eur)
+        sgd = try container.decode(TypeCurrency.self, forKey: .sgd)
+    }
+    
 }
 
 struct TypeCurrency: Decodable {
-    let CharCode: String
-    let Nominal: Int
-    let Name: String
-    let Value: Double
+    
+    let charCode: String
+    let nominal: Int
+    let name: String
+    let value: Double
+    
+    enum CodingKeys: String, CodingKey {
+        case charCode = "CharCode"
+        case nominal = "Nominal"
+        case name = "Name"
+        case value = "Value"
+    }
+    
+    init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        charCode = try container.decode(String.self, forKey: .charCode)
+        nominal = try container.decode(Int.self, forKey: .nominal)
+        name = try container.decode(String.self, forKey: .name)
+        value = try container.decode(Double.self, forKey: .value)
+    }
+    
 }
 
